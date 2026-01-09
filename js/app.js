cc9114588d91145ecfb816f94df5ea75d508a63d
Wiso202 <memegnonlewis@icloud.com>
2026-01-09 19:43:34 +0100

    app.js

--- js/app.js
+++ js/app.js
@@ -33,8 +33,18 @@ async function fetchEvents() {
 
         allEvents = await response.json();
         
+       // --- NOUVEAU : FILTRE DES ÉVÉNEMENTS PASSÉS ---
+        const today = new Date();
+        today.setHours(0, 0, 0, 0); // On garde uniquement la date du jour sans l'heure
+
+        allEvents = data.filter(event => {
+            const eventDate = new Date(event.date);
+            return eventDate >= today; // On ne garde que si la date est aujourd'hui ou dans le futur
+        });
+        // ----------------------------------------------
+
         if (allEvents.length === 0) {
-            grid.innerHTML = '<p class="text-center w-100">Aucun événement disponible pour le moment.</p>';
+            grid.innerHTML = '<p class="text-center w-100">Aucun événement à venir pour le moment.</p>';
         } else {
             displayPage(1);
         }
@@ -215,6 +225,7 @@ function addToCalendar(event) {
     
     window.open(googleCalendarUrl, '_blank');
 }
+
 
 
 
